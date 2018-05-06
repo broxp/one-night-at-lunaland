bb6cb58f514eaa7630725e03d056fe7a94c2ec8d
Karsten <karsten@snapcardster.com>
2018-05-06 18:48:42 +0200

    added decision logic, luna flies now

---
+++ Assets/GameLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour
+{
+	public Text txt;
+	public double hp, ammo, safety;
+	public double safetyDelta;
+	public List<GameObject> toggelable;
+	public bool isOnDecision;
+
+	void Start ()
+	{
+	}
+
+	void Update ()
+	{
+		safety += Time.deltaTime * safetyDelta;
+
+		txt.text = "HP: " + hp + "\n"
+		+ "Ammo: " + ammo + "\n"
+		+ "Safety: " + safety + "\n"
+		+ "isOnDecision: " + isOnDecision;
+
+
+		if (isOnDecision && Input.GetKeyDown ("space")) {
+			foreach (var item in toggelable) {
+				foreach (var subItem in item.GetComponentsInChildren<Collider2D> ()) {
+					subItem.enabled = !subItem.enabled;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/ItemPickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour {
+	public GameLogic game;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		print ("item picked up");
+		game.ammo++;
+		Destroy (this);
+	}
+
+}
