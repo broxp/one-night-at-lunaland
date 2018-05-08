fd8648aa222bb23ad2bf3bc28be9e145da4ea5b6
jan-lukas-bichel <jlbichel@gmail.com>
2018-05-08 11:35:27 +0200

    Added Dialogue System

---
+++ Assets/CanvasView.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasView : MonoBehaviour {
+
+    public Text statusText;
+    public double hp, ammoCarriedByTeddy, ammo, safety;
+    public static CanvasView instance = null;
+    public GameObject PickUpItemBubble, LeaveItemBubble, UpBubble, DownBubble;
+
+    private void Update()
+    {
+        UpdateStatusText();
+    }
+
+    //Initialisieren als Singleton
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //UI für Wegekreuzung
+    public GameObject ShowItemBubble()
+    {
+        GameObject pickUpBubble = GameObject.Instantiate(PickUpItemBubble, transform);
+        GameObject leaveItemBubble = GameObject.Instantiate(LeaveItemBubble, transform);
+
+        pickUpBubble.transform.position += new Vector3(200, 0, 0);
+        leaveItemBubble.transform.position += new Vector3(-200, 0, 0);
+
+        return pickUpBubble;
+    }
+
+    //UI für Item-Pickup
+    public void ShowCrossingBubble()
+    {
+        GameObject upBubble = GameObject.Instantiate(UpBubble, transform);
+        GameObject downBubble = GameObject.Instantiate(DownBubble, transform);
+
+        upBubble.transform.position += new Vector3(0, 200, 0);
+        downBubble.transform.position += new Vector3(0, -200, 0);
+    }
+
+    //Löscht alle UI-Elemente mit dem "Bubble"-Tag (also alle Sprechblasen)
+    public void ClearBubbles()
+    {
+        foreach (Transform child in transform)
+        {
+            if(child.gameObject.tag == "Bubble")
+            {
+                GameObject.Destroy(child.gameObject);
+            }            
+        }
+    }
+
+    //Status-Text Updaten
+    void UpdateStatusText()
+    {
+        statusText.text = "HP: " + hp + "\n"
+        + "ammoCarriedByTeddy: " + ammoCarriedByTeddy + "\n"
+        + "Ammo: " + ammo + "\n"
+        + "Safety: " + safety + "\n";
+    }
+
+}
--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -6,39 +6,92 @@ using UnityEngine.UI;
 
 public class GameLogic : MonoBehaviour
 {
-	public Text txt;
-	public double hp, ammoCarriedByTeddy, ammo, safety;
-	public double safetyDelta;
+    public static GameLogic instance = null;
+
+	public double ammo, monsterDmg, safety, maxSafety, safetyDelta, phaseTwoThreshold;
+    private bool phaseTwoTriggered = false;
 	public List<GameObject> toggelable;
-	public bool isOnDecision;
 
 	public GameObject luna, teddy;
 	public float charactersOffset;
-	void Start ()
+
+    //Initialisieren als Singleton
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update ()
 	{
+        //UNTRIGGER PHASE TWO
+        if(safety <= phaseTwoThreshold && !phaseTwoTriggered)
+        {
+            MonsterPhase2();
+        }
+
+        safety += Time.deltaTime * safetyDelta;
+
+        UpdateCanvasView();
+        UpdateLunaPosition();
+
 	}
 
-	void Update ()
-	{
-		safety += Time.deltaTime * safetyDelta;
+    //Verschiebt Luna entsprechend der Teddy-Pos
+    void UpdateLunaPosition()
+    {
+        Vector3 lunaPos = luna.transform.position;
+        lunaPos.x = teddy.transform.position.x + charactersOffset;
+        luna.transform.position = lunaPos;
+    }
 
-		var lunaPos = luna.transform.position;
-		lunaPos.x = teddy.transform.position.x + charactersOffset;
-		luna.transform.position = lunaPos;
+    //Aktiviert alle Levelelemente die nach oben führen
+    public void ActivateUpWays()
+    {
+        foreach (var item in toggelable)
+        {
+            foreach (var subItem in item.GetComponentsInChildren<Collider2D>())
+            {
+                subItem.enabled = true;
+            }
+        }
+    }
 
-		txt.text = "HP: " + hp + "\n"
-		+ "ammoCarriedByTeddy: " + ammoCarriedByTeddy + "\n"
-		+ "Ammo: " + ammo + "\n"
-		+ "Safety: " + safety + "\n"
-		+ "isOnDecision: " + isOnDecision;
+    //Deaktiviert alle Levelelemente die nach oben führen
+    public void DeactivateUpWays()
+    {
+        foreach (var item in toggelable)
+        {
+            foreach (var subItem in item.GetComponentsInChildren<Collider2D>())
+            {
+                subItem.enabled = false;
+            }
+        }
+    }
 
+    //CanvasView aktualisieren
+    void UpdateCanvasView()
+    {
+        CanvasView.instance.safety = safety;
+        CanvasView.instance.ammo = ammo;
+    }
 
-		if (isOnDecision && Input.GetKeyDown ("space")) {
-			foreach (var item in toggelable) {
-				foreach (var subItem in item.GetComponentsInChildren<Collider2D> ()) {
-					subItem.enabled = !subItem.enabled;
-				}
-			}
-		}
-	}
+    //Wird einmal ausgeführt, wenn Phase 2 startet
+    void MonsterPhase2()
+    {
+        //SOUND WECHSELN
+    }
+
+    public void ResetToCheckpoint()
+    {
+        //LUNA STIRBT
+    }
 }
