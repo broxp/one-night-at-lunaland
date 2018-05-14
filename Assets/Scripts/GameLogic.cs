e95e86e0557efc11bd0828579fe92606037b6858
Johannes <johanneswolf97@gmail.com>
2018-05-14 20:18:21 +0200

    Revert "Revert "small changes""
    This reverts commit 6cf1dc11a4175d1e9e0fc96341fe9432fdd0a8e6.

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -9,7 +9,13 @@ public class GameLogic : MonoBehaviour
 {
     public static GameLogic instance = null;
 
-	public double ammo, monsterDmg, safety, maxSafety, safetyDelta, phaseTwoThreshold, phaseTwoRepelThreshold;
+    public int ammo = 0;
+    public double safety;
+    public double safetyDelta;
+    public double maxSafety = 40; 
+    public double phaseTwoRepelThreshold;
+    public double phaseTwoThreshold;
+    public double monsterDmg;
     public GameObject[] toggelables;
 	public GameObject luna, teddy;
 	public float charactersOffset;
@@ -35,6 +41,7 @@ public class GameLogic : MonoBehaviour
     //Alle Rampen in die Liste aufnehmen
     private void Start()
     {
+        safety = maxSafety;
         toggelables = GameObject.FindGameObjectsWithTag("UpRamp");
         phaseTwoTriggered = false;
         gamePaused = false;
