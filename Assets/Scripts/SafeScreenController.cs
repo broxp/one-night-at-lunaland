c34b7b66b877e7740a4fdb58c207c7ddfe34bd25
jan-lukas-bichel <jlbichel@gmail.com>
2018-05-14 12:02:22 +0200

    Implemented Character Movement for 2.5D Passages

---
+++ Assets/Scripts/SafeScreenController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SafeScreenController : MonoBehaviour {
+
+    float xInput, yInput;
+    SafeScreenModell safeScreenModell;
+
+	void Start ()
+    {
+        safeScreenModell = GetComponent<SafeScreenModell>();
+	}
+	
+	void Update ()
+    {
+		xInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        yInput = CrossPlatformInputManager.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        safeScreenModell.Move(xInput, yInput);
+    }
+}
