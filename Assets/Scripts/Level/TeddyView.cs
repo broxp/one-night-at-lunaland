8fd5ff7d49a93b7de88eeb81f2a820be1904a972
jan-lukas-bichel <jlbichel@gmail.com>
2018-05-15 12:10:20 +0200

    Added Spine Animation-Package

---
+++ Assets/Scripts/Level/TeddyView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class TeddyView : MonoBehaviour {
+
+    SkeletonAnimation skeletanAnimation;
+
+	void Start () {
+        skeletanAnimation = GetComponent<SkeletonAnimation>();
+	}
+	
+	void Update () {
+		
+	}
+}
