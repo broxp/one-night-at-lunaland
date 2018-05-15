b7be326b8aa197d8ee2a8e14532aefd2bc52e538
jan-lukas-bichel <jlbichel@gmail.com>
2018-05-15 12:22:30 +0200

    Added General Trigger

---
+++ Assets/Scripts/TriggerModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TriggerModel : MonoBehaviour {
+
+    public enum EventType { Audio, Scene};
+
+    public EventType eventType;
+    public string EventName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(eventType == EventType.Audio)
+        {
+            AudioModell.instance.PlayAudio(EventName);
+            Destroy(this);
+        }
+        else if(eventType == EventType.Scene)
+        {
+            SceneManager.LoadScene(EventName);
+        }
+    }
+}
