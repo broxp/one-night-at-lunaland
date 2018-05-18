26faa1526778037f36a33893fc3a9a997d2c9458
jan-lukas-bichel <jlbichel@gmail.com>
2018-05-18 20:23:40 +0200

    Folders Renamed, save4b implemented

---
+++ Assets/Scripts/Save4.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Save4 : MonoBehaviour {
+
+    public string nextScene;
+    public AudioClip clip;
+
+	void Start () {
+        StartCoroutine(NextScene());
+	}
+
+    IEnumerator NextScene()
+    {
+        yield return new WaitForSeconds(clip.length);
+        SceneManager.LoadScene(nextScene);
+    }
+}
